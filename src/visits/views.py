6886298be62ec8734a64a496ca2aa2f7b55ceb97
CodingForEntrepreneurs <hello@teamcfe.com>
2024-12-17 16:05:05 -0700

    8 - Multi-Tenant Migrating with Neon Branching

--- src/visits/views.py
+++ src/visits/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def page_visit_view(request):
-    PageVisit.objects.create(path='/', subdomain='www')
-    return HttpResponse("Hello")
+# def page_visit_view(request):
+#     PageVisit.objects.create(path='/', subdomain='www')
+#     return HttpResponse("Hello")
