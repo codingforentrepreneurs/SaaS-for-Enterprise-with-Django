7408bd60d611823aecdb50747f14b6ab20a77c04
CodingForEntrepreneurs <hello@teamcfe.com>
2024-12-19 13:43:53 -0700

    20 - Schema Activation and Exists Functions

--- src/helpers/db/schemas.py
+++ src/helpers/db/schemas.py
@@ -6,15 +6,43 @@ from helpers.db import statements as db_statements
 
 DEFAULT_SCHEMA = "public"
 
+
+def check_if_schema_exists(schema_name, require_check=False):
+    if schema_name == DEFAULT_SCHEMA and not require_check:
+        return True
+    exists = False
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT schema_name 
+            FROM information_schema.schemata 
+            WHERE schema_name = %s
+        """, [schema_name])
+        exists = cursor.fetchone() is not None
+    return exists
+
+
+def activate_tenent_schema(schema_name):
+    is_check_exists_required = schema_name != DEFAULT_SCHEMA
+    schema_to_use = DEFAULT_SCHEMA
+    if is_check_exists_required and check_if_schema_exists(schema_name):
+        schema_to_use = schema_name
+    with connection.cursor() as cursor:
+        sql = f'SET search_path TO "{schema_to_use}";'
+        cursor.execute(sql)
+
+
 @contextmanager
-def use_public_schema():
+def use_public_schema(revert_schema_name=None, revert_schema=False):
     """
     with use_public_schema():
         Tenant.object.all()
     """
     try:
+        schema_to_use = DEFAULT_SCHEMA
         with connection.cursor() as cursor:
-            cursor.execute(db_statements.ACTIVATE_SCHEMA_SQL.format(schema_name=DEFAULT_SCHEMA))
+            sql = f'SET search_path TO "{schema_to_use}";'
+            cursor.execute(sql)
         yield
     finally:
-        print("something")
+        if revert_schema:
+            activate_tenent_schema(revert_schema_name)
--- src/helpers/middleware/schemas.py
+++ src/helpers/middleware/schemas.py
@@ -2,7 +2,8 @@ from django.apps import apps
 from django.db import connection
 
 from helpers.db.schemas import (
-    use_public_schema
+    use_public_schema,
+    activate_tenent_schema
 )
 from helpers.db import statements as db_statements
 
@@ -19,23 +20,8 @@ class SchemaTenantMiddleware:
         if len(host_split) > 1:
             subdomain = host_split[0]
         schema_name = self.get_schema_name(subdomain=subdomain)
-        self.set_search_path(schema_name)
+        activate_tenent_schema(schema_name)
         return self.get_response(request)
-    
-    def set_search_path(self, schema_name):
-        print("activate the schema", schema_name)
-        with connection.cursor() as cursor:
-            cursor.execute("""
-                SELECT schema_name 
-                FROM information_schema.schemata 
-                WHERE schema_name = %s
-            """, [schema_name])
-            schema_exists = bool(cursor.fetchone())
-
-            if not schema_exists:
-                schema_name = "public"
-            cursor.execute(db_statements.ACTIVATE_SCHEMA_SQL.format(schema_name=schema_name))
-        return 
     
     def get_schema_name(self, subdomain=None):
         if subdomain in [None, "localhost", 'desalsa']:
