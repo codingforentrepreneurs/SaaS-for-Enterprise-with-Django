a504f6c2500e55c55e3fb3c1f27ddcb98ca9fc60
CodingForEntrepreneurs <hello@teamcfe.com>
2024-12-19 17:02:45 -0700

    23 - Multi-Tenant User Registration

--- src/customers/models.py
+++ src/customers/models.py
@@ -1,7 +1,8 @@
 import helpers.billing
 from django.conf import settings
-from django.db import models
+from django.db import models, connection
 
+# database signals
 from allauth.account.signals import (
     user_signed_up as allauth_user_signed_up,
     email_confirmed as allauth_email_confirmed
@@ -9,6 +10,8 @@ from allauth.account.signals import (
 
 User = settings.AUTH_USER_MODEL # "auth.user"
 
+# public schema -> customer accounts
+# enterprise schema no customer account
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     stripe_id = models.CharField(max_length=120, null=True, blank=True)
@@ -35,28 +38,30 @@ class Customer(models.Model):
 
 
 def allauth_user_signed_up_handler(request, user, *args, **kwargs):
-    email = user.email
-    Customer.objects.create(
-        user=user,
-        init_email=email,
-        init_email_confirmed=False,
-    )
+    if connection.schema_name == "public":
+        email = user.email
+        Customer.objects.create(
+            user=user,
+            init_email=email,
+            init_email_confirmed=False,
+        )
 
 allauth_user_signed_up.connect(allauth_user_signed_up_handler)
 
 
 def allauth_email_confirmed_handler(request, email_address, *args, **kwargs):
-    qs = Customer.objects.filter(
-        init_email=email_address,
-        init_email_confirmed=False,
-    )
-    # does not send the save method or create the
-    # stripe customer
-    # qs.update(init_email_confirmed=True)
-    for obj in qs:
-        obj.init_email_confirmed=True
-        # send the signal
-        obj.save()
+    if connection.schema_name == "public":
+        qs = Customer.objects.filter(
+            init_email=email_address,
+            init_email_confirmed=False,
+        )
+        # does not send the save method or create the
+        # stripe customer
+        # qs.update(init_email_confirmed=True)
+        for obj in qs:
+            obj.init_email_confirmed=True
+            # send the signal
+            obj.save()
 
 
 allauth_email_confirmed.connect(allauth_email_confirmed_handler)
