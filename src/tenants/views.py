5d73f9de8f0f8a6bf65d592adcf8d1093c403b0a
CodingForEntrepreneurs <hello@teamcfe.com>
2024-12-20 16:15:21 -0700

    29 - Register Enterprise-only User in Public Django Schema

--- src/tenants/views.py
+++ src/tenants/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 
 from django.contrib.auth import get_user_model
+
+from allauth.account.forms import SignupForm
 
 from helpers.db.schemas import (
     use_tenant_schema
@@ -35,3 +38,35 @@ def tenant_detail_view(request, pk):
         "enterprise_users": enterprise_users
     }
     return render(request, "tenants/detail.html", context)
+
+
+@login_required
+def tenant_create_user_view(request, pk):
+    # public schema -> update urls.py -> django-hosts
+    owner = request.user
+    instance = get_object_or_404(Tenant, owner=owner, pk=pk)
+    form = SignupForm()
+    with use_tenant_schema(instance.schema_name, create_if_missing=True, revert_public=True):
+        form = SignupForm(request.POST or None)
+        if form.is_valid():
+            form.save(request)
+            pk = instance.pk
+            return redirect(f'/tenants/{pk}')
+    context = {
+        "object": instance,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "tenants/new-user.html", context)
+
+
+@login_required
+def tenant_user_detail_view(request, tenant_pk, user_pk):
+    # public schema -> update urls.py -> django-hosts
+    owner = request.user
+    instance = get_object_or_404(Tenant, owner=owner, pk=tenant_pk)
+    user_instance = None
+    with use_tenant_schema(instance.schema_name, create_if_missing=True, revert_public=True):
+        user_instance = User.objects.get(pk=user_pk)
+        return HttpResponse(f"{user_instance.id}-{user_instance.username}")
+    return HttpResponse("")
