3fe1c3b8571120ffe71c95a2b2d96f7bc7bca178
CodingForEntrepreneurs <hello@teamcfe.com>
2024-12-20 15:32:48 -0700

    27 - Tenant List and Detail

--- src/tenants/admin.py
+++ src/tenants/admin.py
@@ -5,5 +5,6 @@ from .models import Tenant
 
 class TenantAdmin(admin.ModelAdmin):
     readonly_fields = ['schema_name', 'active_at', 'inactive_at', 'timestamp', 'updated' ]
+    list_display = ['subdomain', 'owner', 'schema_name']
 
 admin.site.register(Tenant, TenantAdmin)
