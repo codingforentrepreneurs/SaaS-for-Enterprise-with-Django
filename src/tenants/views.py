e0493704f996643fbbc75471d0bd7a15ad406b1d
CodingForEntrepreneurs <hello@teamcfe.com>
2024-12-20 15:58:27 -0700

    28 - Display Tenant Users in Public Schema Django View

--- src/tenants/views.py
+++ src/tenants/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render, get_object_or_404
 
 from django.contrib.auth import get_user_model
 
+from helpers.db.schemas import (
+    use_tenant_schema
+)
+
 from .models import Tenant
 
 User = get_user_model()
@@ -18,10 +22,16 @@ def tenant_list_view(request):
 
 @login_required
 def tenant_detail_view(request, pk):
+    # public schema -> update urls.py -> django-hosts
     owner = request.user
     instance = get_object_or_404(Tenant, owner=owner, pk=pk)
+    enterprise_users = []
+    with use_tenant_schema(instance.schema_name, create_if_missing=True, revert_public=True):
+        # cache?? -> per tenant caching system setup too
+        enterprise_users = list(User.objects.all())
     context = {
         "object": instance,
         "instance": instance,
+        "enterprise_users": enterprise_users
     }
     return render(request, "tenants/detail.html", context)
